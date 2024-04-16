596dc63cd8662521d0d116514b90c0d15fbee2e0
ratedaron <abh.robo@gmail.com>
2024-04-16 13:42:16 +0200

    v1

--- todo/todo/src/main/java/wiss/todoApp/controller/PublicController.java
+++ todo/todo/src/main/java/wiss/todoApp/controller/PublicController.java
@@ -2,14 +2,13 @@ package wiss.todoApp.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // @Controller wird automatisch aufgerufn und man bracuh es noicht manuel mit
 // TOTO apllikation verbinfen
 public class PublicController {
 
-    @RequestMapping("/public")
+    @GetMapping("/public")
     public ResponseEntity<String> getGreeting() {
         return ResponseEntity.ok("Public content");
     }
